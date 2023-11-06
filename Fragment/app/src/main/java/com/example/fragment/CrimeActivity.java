d09347148c8ecd9f80d192f567e70242d9ff8531
varenik-1 <vs90288vs@gmail.com>
2023-11-06 14:42:09 +0300

    4to-to po 7 glave

---
+++ Fragment/app/src/main/java/com/example/fragment/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.example.fragment;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
