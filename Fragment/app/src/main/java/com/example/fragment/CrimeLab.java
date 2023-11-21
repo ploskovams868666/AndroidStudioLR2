6ae82554b4b6af74cbbad7c56bf2caeccdf027fc
ploskovams868666 <kit.mariya2018@yandex.ru>
2023-11-21 18:37:03 +0300

    err DB

--- Fragment/app/src/main/java/com/example/fragment/CrimeLab.java
+++ Fragment/app/src/main/java/com/example/fragment/CrimeLab.java
@@ -1,67 +1,26 @@
 package com.example.fragment;
-
 import android.content.ContentValues;
+
+import java.io.File;
+import java.util.UUID;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import java.util.ArrayList;
+import java.util.List;
 import com.example.fragment.database.CrimeBaseHelper;
 import com.example.fragment.database.CrimeCursorWrapper;
 import com.example.fragment.database.CrimeDbSchema;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import com.example.fragment.database.CrimeDbSchema.CrimeTable;
+
 
 public class CrimeLab {
-    private static CrimeLab sCrimeLab;
+
     private Context mContext;
+    private static CrimeLab sCrimeLab;
     private SQLiteDatabase mDatabase;
-    //private Crime mCrime;
-    //private File mPhotoFile;
-    //private EditText mTitleField;
-
-    /*@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
-        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
-        mPhotoFile = CrimeLab.get(getActivity()).getPhotoFile(mCrime);
-    }*/
-    // мб из другого файла
 
 
-    public static CrimeLab get(Context context) {
-        if (sCrimeLab == null) {
-            sCrimeLab = new CrimeLab(context);
-        }
-        return sCrimeLab;
-    }
-
-    private CrimeLab(Context context) {
-        mContext = context.getApplicationContext();
-        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
-    }
-
-    public void addCrime(Crime c) {
-        ContentValues values = getContentValues(c);
-        mDatabase.insert(CrimeDbSchema.CrimeTable.NAME, null, values);
-    }
-
-    public List<Crime> getCrimes() {
-
-        List<Crime> crimes = new ArrayList<>();
-        CrimeCursorWrapper cursor = queryCrimes(null, null);
-        try {
-            cursor.moveToFirst();
-            while (!cursor.isAfterLast()) {
-                crimes.add(cursor.getCrime());
-                cursor.moveToNext();
-            }
-        } finally {
-            cursor.close();
-        }
-        return crimes;
-    }
 
     public Crime getCrime(UUID id) {
         CrimeCursorWrapper cursor = queryCrimes(
@@ -79,24 +38,18 @@ public class CrimeLab {
         }
     }
 
-    public File getPhotoFile(Crime crime) {
-        File filesDir = mContext.getFilesDir();
-        return new File(filesDir, crime.getPhotoFilename());
-        //if (externalFilesDir == null) {}
-    }
-
     public void updateCrime(Crime crime) {
         String uuidString = crime.getId().toString();
         ContentValues values = getContentValues(crime);
-        mDatabase.update(CrimeDbSchema.CrimeTable.NAME, values,
-                CrimeDbSchema.CrimeTable.Cols.UUID + " = ?",
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
                 new String[]{uuidString});
     }
 
     private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs) {
         Cursor cursor = mDatabase.query(
-                CrimeDbSchema.CrimeTable.NAME,
-                null,
+                CrimeTable.NAME,
+                null, // columns - с null выбираются все столбцы
                 whereClause,
                 whereArgs,
                 null, // groupBy
@@ -108,12 +61,48 @@ public class CrimeLab {
 
     private static ContentValues getContentValues(Crime crime) {
         ContentValues values = new ContentValues();
-        values.put(CrimeDbSchema.CrimeTable.Cols.UUID, crime.getId().toString());
-        values.put(CrimeDbSchema.CrimeTable.Cols.TITLE, crime.getTitle());
-        values.put(CrimeDbSchema.CrimeTable.Cols.DATE, crime.getDate().getTime());
-        values.put(CrimeDbSchema.CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
-        values.put(CrimeDbSchema.CrimeTable.Cols.SUSPECT, crime.getSuspect());
+        values.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        values.put(CrimeTable.Cols.SUSPECT, crime.getSuspect());
         return values;
     }
-}
 
+
+    public void addCrime(Crime c) {
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeDbSchema.CrimeTable.NAME, null, values);
+    }
+
+    public List<Crime> getCrimes() {
+        List<Crime> crimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+        return crimes;
+    }
+
+    private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+    }
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
+    }
+}
--- Fragment/app/src/main/java/com/example/fragment/database/CrimeBaseHelper.java
+++ Fragment/app/src/main/java/com/example/fragment/database/CrimeBaseHelper.java
@@ -8,18 +8,26 @@ import com.example.fragment.database.CrimeDbSchema.CrimeTable;
 public class CrimeBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "crimeBase.db";
-
-    public CrimeBaseHelper(Context context){
+    public CrimeBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL("create table " + CrimeTable.NAME + "(" + " _id integer primary key autoincrement, " + CrimeTable.Cols.UUID + ", " + CrimeTable.Cols.TITLE + ", " + CrimeTable.Cols.DATE + ", " + CrimeTable.Cols.Solved + CrimeTable.Cols.SUSPECT + ")");
-    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + CrimeTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
 
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+
+                CrimeTable.Cols.SOLVED + ", " +
+                CrimeTable.Cols.SUSPECT +
+                ")"
+        );
+    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
     }
+
 }
