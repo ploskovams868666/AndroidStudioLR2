6575343f340891abc24e6b7bc236efd77eee28b7
ploskovams868666 <kit.mariya2018@yandex.ru>
2023-11-24 19:09:43 +0300

    vse

--- Fragment/app/src/main/java/com/example/fragment/database/CrimeBaseHelper.java
+++ Fragment/app/src/main/java/com/example/fragment/database/CrimeBaseHelper.java
@@ -14,17 +14,7 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + CrimeTable.NAME + "(" +
-                " _id integer primary key autoincrement, " +
-
-                CrimeTable.Cols.UUID + ", " +
-                CrimeTable.Cols.TITLE + ", " +
-                CrimeTable.Cols.DATE + ", " +
-
-                CrimeTable.Cols.SOLVED + ", " +
-                CrimeTable.Cols.SUSPECT +
-                ")"
-        );
+        db.execSQL("create table " + CrimeTable.NAME + "(" + " _id integer primary key autoincrement, " + CrimeTable.Cols.UUID + ", " + CrimeTable.Cols.TITLE + ", " + CrimeTable.Cols.DATE + ", " + CrimeTable.Cols.SOLVED + ", " + CrimeTable.Cols.SUSPECT + ")");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
